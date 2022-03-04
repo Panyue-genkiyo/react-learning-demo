486223d4c37f0eef575315f32284d4aa7b6fd0ce
panyue <90938238g@gmail.com>
2022-03-04 22:50:15 +0800

    主页

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import { useState, useEffect } from "react";
-import axios from 'axios';
+import React, {useEffect, useState, useMemo} from 'react';
 import Products from "../components/Products";
+import useQuery from "../hooks/useQuery";
+import Pagination from "../components/Pagination";
 
 const Home = () => {
     //状态
+    const [ limit, setLimit ] = useState(5);
+    const [ page, setPage ] = useState(1);
     const [products, setProducts] = useState([]);
 
+    const { data, loading, error } = useQuery(
+        `/products?limit=${limit}&page=${page}`
+    );
+
     useEffect(() => {
-        axios.get('/products').then(res => {
-            console.log(res.data)
-            // setProducts(res.data);
-            setProducts(res.data.data.products);
-        });
-    }, [])
+        if(data?.products) setProducts(data.products);
+    }, [data?.products]);
+
+    const totalPages = useMemo(() => {
+        if(!data?.count) return 0;
+        return Math.ceil(data.count / limit);
+    },[data?.count]);
 
     return (
         <div>
             <Products products={products}/>
+            { loading && <h2>loading...</h2> }
+            { error && <h2>{error}</h2> }
+            <Pagination totalPages={totalPages} page={page}/>
         </div>
     );
 };
