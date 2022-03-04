558a5e540c3e307a4073188a6081b52e529dfee5
panyue <90938238g@gmail.com>
2022-03-04 22:08:03 +0800

    主组件(主要配置路由信息)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import Home from "./page/Home";
+import ProductDetail from "./page/ProductDetail";
 
 const App = () => {
     return (
         <div>
            <Routes>
                <Route path="/" element={<Home/>} />
-               <Route path="/products" element={<Home/>} />
+               <Route path="/products" element={<Home/>}/>
+               <Route path="/products/:id" element={<ProductDetail/>}/>
            </Routes>
         </div>
     );
