614aee5730fea5558de8bab479fbd2e01033c703
panyue <90938238g@gmail.com>
2022-03-05 00:00:39 +0800

    主页

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,14 +1,15 @@
 import React, {useEffect, useState, useMemo} from 'react';
+import { useLocation } from "react-router-dom";
 import Products from "../components/Products";
 import useQuery from "../hooks/useQuery";
 import Pagination from "../components/Pagination";
 
 const Home = () => {
     //状态
-    const [ limit, setLimit ] = useState(5);
+    const [ limit, setLimit ] = useState(9);
     const [ page, setPage ] = useState(1);
-    const [products, setProducts] = useState([]);
-
+    const [ products, setProducts ] = useState([]);
+    const { search } = useLocation();
     const { data, loading, error } = useQuery(
         `/products?limit=${limit}&page=${page}`
     );
@@ -21,6 +22,11 @@ const Home = () => {
         if(!data?.count) return 0;
         return Math.ceil(data.count / limit);
     },[data?.count]);
+
+    useEffect(() => {
+        const page =  new URLSearchParams(search).get('page') || 1;
+        setPage(+page);
+    }, [search])
 
     return (
         <div>
