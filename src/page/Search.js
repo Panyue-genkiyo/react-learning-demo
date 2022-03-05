99c436effbf3d0ee4844ed2c9fe7f41d096bb0c1
panyue <90938238g@gmail.com>
2022-03-05 17:10:53 +0800

    展示查找结果页面

---
+++ src/page/Search.js
@@ -0,0 +1,27 @@
+//search页面
+import React, { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+import useQuery from "../hooks/useQuery";
+import Products from "../components/Products";
+
+const Search = () => {
+
+    const { value } = useParams();
+    const [products, setProducts] = useState([]);
+
+    const { data, loading, error } = useQuery(`/products?search=${value}`);
+
+    useEffect(() => {
+       if(data?.products) setProducts(data?.products);
+    },[data?.products]);
+
+    return (
+        <>
+            <Products products={products}/>
+            { loading && <h2>loading...</h2> }
+            { error && <h2>{error}</h2> }
+        </>
+    );
+};
+
+export default Search;
