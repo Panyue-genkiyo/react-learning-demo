4ddb823d9602083f74b955657358a780291fed75
panyue <90938238g@gmail.com>
2022-03-05 17:11:12 +0800

    search表单组件

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import { useNavigate } from "react-router-dom";
 
 const SearchForm = () => {
+
+    const inputRef = useRef();
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const value = inputRef.current.value;
+        if(!value.trim()) return;
+        return navigate(`/search/${value}`);
+    }
+
     return (
         <div className='search_form'>
-            <form>
-                <input type='text'/>
+            <form onSubmit={handleSubmit}>
+                <input type='text' ref={inputRef}/>
                 <button>Search</button>
             </form>
         </div>
