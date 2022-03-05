dd8b3d8d3b129bbea624d9df60c2759ecced45fe
panyue <90938238g@gmail.com>
2022-03-05 16:35:16 +0800

    search表单组件

---
+++ src/components/SearchForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SearchForm = () => {
+    return (
+        <div className='search_form'>
+            <form>
+                <input type='text'/>
+                <button>Search</button>
+            </form>
+        </div>
+    );
+};
+
+export default SearchForm;
