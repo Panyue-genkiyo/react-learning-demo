b0480bf6958acc8d985cd144350117f5a6c27442
panyue <90938238g@gmail.com>
2022-03-05 15:05:16 +0800

    分页组件

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,9 +2,9 @@ import React  from 'react';
 import usePagination from "../hooks/usePagination";
 
 //react memo避免home主页渲染多次导致pagination组件多次无效渲染
-const Pagination = ({ totalPages, page }) => {
+const Pagination = ({ totalPages, page, sort }) => {
 
-    const { prev, next, jump, firstArr, lastArr, isActive } = usePagination(totalPages, page);
+    const { prev, next, jump, firstArr, lastArr, isActive } = usePagination(totalPages, page, sort);
 
     return (
         <div className='pagination'>
