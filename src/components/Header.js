52e3123ca2ea7bd195c59fc4165d70e4970d8cf6
panyue <90938238g@gmail.com>
2022-03-05 20:05:36 +0800

    头部组件

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react';
 import Model from "./Model";
 import SearchForm from "./SearchForm";
 import FilterForm from "./FilterForm";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
     const [ openSearch, setOpenSearch ] = useState(false);
     const [ openFilter, setOpenFilter ] = useState(false);
+    const navigate = useNavigate();
     return (
         <header>
             <nav>
-                <p>Home</p>
+                <p onClick={() => navigate('/')}>Home</p>
                 <p>Create Product</p>
                 <p onClick={() => setOpenSearch(true)}>Search</p>
                 <p onClick={() => setOpenFilter(true)}>Filter</p>
@@ -17,14 +19,14 @@ const Header = () => {
             {
                 openSearch && (
                     <Model titleTxt={'search'} setOpen={setOpenSearch}>
-                        <SearchForm/>
+                        <SearchForm setOpen={setOpenSearch}/>
                     </Model>
                 )
             }
             {
                 openFilter && (
                     <Model titleTxt={'filter'} setOpen={setOpenFilter}>
-                        <FilterForm/>
+                        <FilterForm setOpen={setOpenFilter}/>
                     </Model>
                 )
             }
