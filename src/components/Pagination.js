fa999e175f47fd629e7e64884b9f04cfafa6cc2a
panyue <90938238g@gmail.com>
2022-03-05 10:01:44 +0800

    分页组件

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,36 +1,23 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React  from 'react';
+import usePagination from "../hooks/usePagination";
 
 const Pagination = ({totalPages, page }) => {
-    const newArr = [...Array(totalPages)].map((_, i) => i + 1);
-    const navigate = useNavigate();
-    const isActive = (index) => {
-        if(index === page) return "active"
-        return ""
-    }
 
-    //上一页
-    const prev = () => {
-        const newPage = Math.max(page - 1, 1)
-        navigate(`?page=${newPage}`)
-    }
-
-    //下一页
-    const next = () => {
-        const newPage = Math.min(page + 1, totalPages)
-        navigate(`?page=${newPage}`)
-    }
-
-    //跳页
-    const jump = (num) => {
-        navigate(`?page=${num}`)
-    }
+    const { prev, next, jump, firstArr, lastArr, isActive } = usePagination(totalPages, page);
 
     return (
         <div className='pagination'>
             <button onClick={prev}>&laquo;</button>
             {
-                newArr.map(num => (
+                firstArr.map(num => (
+                    <button onClick={() => jump(num)} key={num} className={`${isActive(num)}`}>
+                        {num}
+                    </button>
+                ))
+            }
+            {lastArr.length > 0 && <button>...</button>}
+            {
+                lastArr.map(num => (
                     <button onClick={() => jump(num)} key={num} className={`${isActive(num)}`}>
                         {num}
                     </button>
