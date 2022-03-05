8dbf3c2b4491e52976eef4dfe14b06512fc7b405
panyue <90938238g@gmail.com>
2022-03-05 15:44:10 +0800

    分页组件(使用自定义hook(useMyContext)读取数据)

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,11 +1,10 @@
-import React  from 'react';
+import React from 'react';
 import usePagination from "../hooks/usePagination";
 
 //react memo避免home主页渲染多次导致pagination组件多次无效渲染
-const Pagination = ({ totalPages, page, sort }) => {
+const Pagination = ({ totalPages }) => {
 
-    const { prev, next, jump, firstArr, lastArr, isActive } = usePagination(totalPages, page, sort);
-
+    const { prev, next, jump, firstArr, lastArr, isActive } = usePagination(totalPages);
     return (
         <div className='pagination'>
             <button onClick={prev}>&laquo;</button>
