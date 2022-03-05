d2e8e063e121b461b486d5f7e4c50efda735586f
panyue <90938238g@gmail.com>
2022-03-05 21:00:06 +0800

    商品组件

--- src/components/ProductsCard.js
+++ src/components/ProductsCard.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
+import Model from "./Model";
+import ProductForm from "./ProductForm";
+import axios from "axios";
 
 const ProductsCard = ( { product } ) => {
+    const [openProduct, setOpenProduct] = useState(false);
+    const handleDelete = (id) => {
+        if(window.confirm('Are you sure you want to delete this product?')) {
+            axios.delete(`/products/${id}`)
+                .then(res => {
+                    console.log(res);
+                    window.location.reload();
+                })
+                .catch(err => console.log(err));
+        }
+    }
     return (
         <div className='card'>
             <img src={product.image} alt={product.image} />
@@ -14,9 +28,16 @@ const ProductsCard = ( { product } ) => {
                 </h3>
                 <h4>${product.price}</h4>
                 <div className='btn_div'>
-                    <button className="btn_edit">Edit</button>
-                    <button className="btn_delete">Delete</button>
+                    <button className="btn_edit" onClick={() => setOpenProduct(true)}>Edit</button>
+                    <button className="btn_delete" onClick={() => handleDelete(product._id)}>Delete</button>
                 </div>
+                {
+                    openProduct && (
+                        <Model titleTxt={'Update Product'} setOpen={setOpenProduct}>
+                            <ProductForm btnTxt={'Update'} setOpen={setOpenProduct} data={product}/>
+                        </Model>
+                    )
+                }
             </div>
         </div>
     );
