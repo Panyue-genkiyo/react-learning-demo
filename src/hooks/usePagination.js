47c0ff1cb1de72f8dd7fc1dcce891637f664262a
panyue <90938238g@gmail.com>
2022-04-10 21:57:06 +0800

    自定义分页hook

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,61 +1,44 @@
-//分页hook
-import React, {useMemo} from 'react';
-import useCustomRouter from "./useCustomRouter";
-import { useMyContext } from "../context/store";
+import { useState, useEffect } from 'react';
+import { useMyContext } from '../context/store';
+import useCustomRouter from './useCustomRouter';
+
 
 const usePagination = (totalPages) => {
-    const { page, sort } = useMyContext();
-    const {firstArr, lastArr} = useMemo(() => {
-        // console.log(totalPages, page);
-        const newArr = [...Array(totalPages)].map((_, i) => i + 1);
-        // console.log(newArr);
-        if (totalPages < 4)
-            return {
-                firstArr: newArr,
-                lastArr: []
-            }; //当总页数小于4时
-        if (totalPages - page >= 4) {
-            return {
-                firstArr: newArr.slice(page - 1, page + 2), //展示前3页
-                lastArr: newArr.slice(totalPages - 1)  //展示最后一页
-            }
-        } else {
-            return {
-                firstArr: newArr.slice(totalPages - 4, totalPages),
-                lastArr: []
-            }
-        }
-    }, [totalPages, page]);
+  const { page, sort } = useMyContext()
+  const [firstArr, setFirstArr] = useState([])
+  const [lastArr, setLastArr] = useState([])
+  const { pushQuery } = useCustomRouter()
 
+  useEffect(() => {
+    const newArr = [...Array(totalPages)].map((_,i) => i + 1)
+    if(totalPages <= 4)
+      return setFirstArr(newArr);
 
-    const {pushQuery} = useCustomRouter();
-    const isActive = (index) => {
-        if (index === page) return "active"
-        return ""
+    if(totalPages - page >= 3) {
+      setFirstArr(newArr.slice(page - 1, page + 2))
+      setLastArr(newArr.slice(totalPages - 1))
+    }else{
+      setFirstArr(newArr.slice(totalPages - 4, totalPages))
+      setLastArr([])
     }
+  },[totalPages, page])
 
-    //上一页
-    const prev = () => {
-        const newPage = Math.max(page - 1, 1)
-        // navigate(`?page=${newPage}`)
-        pushQuery({page: newPage, sort});
-    }
-
-    //下一页
-    const next = () => {
-        const newPage = Math.min(page + 1, totalPages)
-        // navigate(`?page=${newPage}`)
-        pushQuery({page: newPage, sort}); //添加排序
-    }
-
-    //跳页
-    const jump = (num) => {
-        // navigate(`?page=${num}`)
-        pushQuery({page: num, sort});
-    }
-
-
-    return {firstArr, lastArr, isActive, prev, next, jump}
+  function next() {
+    const newPage = Math.min(page + 1, totalPages)
+    pushQuery({page: newPage, sort})
+  }
+    
+  function prev() {
+    const newPage = Math.max(page - 1, 1)
+    pushQuery({page: newPage, sort})
+  }
+    
+  function jump(page) {
+    const newPage = Math.max(1, page)
+    pushQuery({page: newPage, sort})
+  }
+  
+  return { page, firstArr, lastArr, next, prev, jump }
 };
 
 export default usePagination;
