5ccd3c89e292a8164cb194e453bdd3f0450ef0fa
panyue <90938238g@gmail.com>
2022-04-15 21:51:07 +0800

    products组件

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ProductCard from './ProductCard';
 
 const Products = React.memo(({ data, loading, error }) => {
-  
+
   if(error) return <h2>{error}</h2>;
   return <section>
     <div className='products'>
       {
-        data.map(product => (
+        data && data.map(product => (
           <ProductCard key={product._id} product={product} />
         ))
       }
     </div>
-    
+
     { loading && <h2 style={{textAlign: 'center'}}>Loading...</h2> }
   </section>;
 });
