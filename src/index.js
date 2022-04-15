74c1509b64821acb022ebb1ac1272bb99ead20b4
panyue <90938238g@gmail.com>
2022-04-15 21:52:44 +0800

    添加QueryClientProvider和ReactQueryDevtools

--- src/index.js
+++ src/index.js
@@ -7,15 +7,22 @@ import axios from 'axios';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import  { ContextProvider } from './context/store'
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
-axios.defaults.baseURL = 'http://localhost:5001/api'
+axios.defaults.baseURL = 'http://localhost:5001/api';
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <ContextProvider>
-        <App />
-        <ToastContainer />
+          <QueryClientProvider client={queryClient}>
+              <App />
+              <ToastContainer />
+              <ReactQueryDevtools position='bottom-right'/>
+          </QueryClientProvider>
       </ContextProvider>
     </BrowserRouter>
    </React.StrictMode>,
