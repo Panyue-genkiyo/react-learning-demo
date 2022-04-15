b502cf74a0690cb8c4efd1708f9c99afa1281c97
panyue <90938238g@gmail.com>
2022-04-15 21:52:09 +0800

    改用useQuery请求单个product的详情数据

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { useParams } from 'react-router-dom'
-import { getOneProduct } from '../api/productAPI';
+import {getData, getOneProduct} from '../api/productAPI';
 import ProductInfo from '../components/ProductInfo';
-import useQuery from '../hooks/useQuery';
+import { useQuery } from "react-query";
 
 
 const ProductDetail = () => {
   const { id } = useParams()
   const url = getOneProduct(id)
-  const { data, loading, error } = useQuery(url)
+  const { data: productDetail, isLoading, isError, error} = useQuery(url, getData)
 
-  return <main>
-    <ProductInfo 
-    product={data} 
-    loading={loading} 
-    error={error}  
-    />
-  </main>;
+  return (
+      <main>
+        <ProductInfo
+            product={productDetail?.data}
+            loading={isLoading}
+            error={isError ? error.message : null}
+        />
+      </main>
+  )
 };
 
 export default ProductDetail;
