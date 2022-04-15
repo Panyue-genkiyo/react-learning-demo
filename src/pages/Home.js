a44f957c783c9c39b7d2adf15512591ec91eea98
panyue <90938238g@gmail.com>
2022-04-15 21:51:35 +0800

    改用useQuery请求products数据

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,42 +1,42 @@
-import React, { useState, useMemo } from 'react';
-import { getProducts } from '../api/productAPI';
-
+import React, { useMemo } from 'react';
 import Pagination from '../components/Pagination';
 import Products from '../components/Products';
 import Sorting from '../components/Sorting';
 import { useMyContext } from '../context/store';
 import useCustomRouter from '../hooks/useCustomRouter';
-import useQuery from '../hooks/useQuery';
+import { useQuery } from 'react-query';
+import { getData } from "../api/productAPI";
 
 const Home = () => {
-  const { refresh, page, limit, sort } = useMyContext()
-  const [products, setProducts] = useState([])
+  const { page, limit, sort } = useMyContext()
 
-  const { pushQuery } = useCustomRouter()
+  const { pushQuery } = useCustomRouter();
 
-  const url = getProducts(limit, page, sort)
-  const { data, loading, error } = useQuery(url, [refresh], {
-    saveCache: true
-  })
+  // const url = getProducts(limit, page, sort);
+
+  const key = `/products?limit=${limit}&page=${page}&sort=${sort}`;
+
+  const { data: productsData, isLoading, error, isError } = useQuery(key, getData)
 
   const totalPages = useMemo(() => {
-    if(!data) return 0;
-    setProducts(() => data.products)
-    return Math.ceil(data.count/limit)
-  }, [data, limit]);
- 
-  
-  return <main>
-    <Sorting sort={sort}
-    calback={(sort) => pushQuery({page, sort})}
-     />
-    <Products 
-    data={products} 
-    loading={loading} 
-    error={error} 
-    />
-    <Pagination totalPages={totalPages}/>
-  </main>;
+    if(!productsData) return 0;
+    return Math.ceil(productsData.data.count/limit)
+  }, [productsData, limit]);
+
+
+  return (
+      <main>
+        <Sorting sort={sort}
+                 calback={(sort) => pushQuery({page, sort})}
+        />
+        <Products
+            data={productsData?.data.products}
+            loading={isLoading}
+            error={isError ? error.message : null}
+        />
+        <Pagination totalPages={totalPages}/>
+      </main>
+  )
 };
 
 export default Home;
