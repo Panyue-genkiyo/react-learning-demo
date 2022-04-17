4bfbdd0ac6afa9581ca45d7c61368aa518ce466e
panyue <90938238g@gmail.com>
2022-04-17 22:37:15 +0800

    探究乐观更新(optimistic update) queryClient.setQueryData(key, (oldData) => {}...)少一次请求

--- src/hooks/useMutationProduct.js
+++ src/hooks/useMutationProduct.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useMutation, useQueryClient } from "react-query";
-import {createProduct, updateProduct, deleteProduct, handleError} from "../api/productAPI";
+import { createProduct, updateProduct, deleteProduct, handleError } from "../api/productAPI";
 import { toast } from "react-toastify";
 
-//使用react-query useMutation hook/更行商品
+//使用react-query useMutation hook/更新商品
 export const useCreateProduct = () => {
     //返回的mutate函数所接受到的参数将会被传到处理函数中(如下就是createProduct)
     const queryClient = useQueryClient();
@@ -27,9 +27,23 @@ export const useUpdateProduct = () => {
     return useMutation(
         updateProduct,
         {
+            onMutate: (data) => {
+                //与上面updateProduct接到参数是一样的注意
+                if(!keys?.k1) return;
+                //optimistic update 更新缓存
+                queryClient.setQueryData(keys?.k1, (oldData) => {
+                    //获取key为keys.k1的cache数据
+                    const products = oldData?.data.products.map(p => (
+                        p._id === data.id ? { ...p, ...data.newData}: p
+                    ));
+                    return { ...oldData, data: {...oldData.data , products }}
+                })
+            },
             onSuccess: _data =>  toast.success('修改商品成功！'),
             onError: error => handleError(error),
-            onSettled: () => queryClient.invalidateQueries(keys?.k1 || keys?.k2)
+            onSettled: () => {
+                if(keys?.k2) queryClient.invalidateQueries(keys?.k2);
+            }
         }
     );
 };
