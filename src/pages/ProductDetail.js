7bd1e548aed596a15a11cdf621a478297febcbb0
panyue <90938238g@gmail.com>
2022-04-17 10:12:29 +0800

    配置placeholder属性跳过useQuery hard loading，注意是fake data并不存在与cache里面

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -2,19 +2,44 @@ import React from 'react';
 import { useParams } from 'react-router-dom'
 import {getData, getOneProduct} from '../api/productAPI';
 import ProductInfo from '../components/ProductInfo';
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 
 
 const ProductDetail = () => {
-  const { id } = useParams()
-  const url = getOneProduct(id)
-  const { data: productDetail, isLoading, isError, error} = useQuery(
+  const { id } = useParams();
+  const url = getOneProduct(id);
+  const queryClient = useQueryClient();
+  const keys = queryClient.getQueryData('keys');
+  // console.log(keys);
+
+  const { data: productDetail, isLoading, isError, error, isPlaceholderData} = useQuery(
       url,
       getData,
       {
-          enabled: !!id //当id不存在是不会触发该useQuery
+          enabled: !!id, //当id不存在是不会触发该useQuery
+          //placeholder: 假数据，不在cache里，跳过loading状态 防止第一次的hard loading状态,改善用户体验
+          placeholderData: () => {
+              if(keys?.k1){
+                  const { data } = queryClient.getQueryData(keys.k1);
+                  return data.products.find(item => item._id === id);
+              }
+              if(keys?.k2){
+                  let product;
+                  const  pages  = queryClient.getQueryData(keys.k2).pages;
+                  pages.map(page => {
+                      return page.data.products.forEach(item => {
+                          if(item._id === id){
+                              product = item;
+                          }
+                      })
+                  })
+                  return product;
+              }
+          }
       }
   )
+
+    console.log({ isPlaceholderData })
 
   return (
       <main>
